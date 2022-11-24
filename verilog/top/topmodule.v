4e5e138887a1471498ca492c80fb38a8485e064d
Kiriki-liszt <kirikiaris@gmail.com>
2022-11-24 17:07:07 +0900

    io

--- verilog/top/topmodule.v
+++ verilog/top/topmodule.v
@@ -1,14 +1,15 @@
 module RNN( gains, vad, feature, clk );
 // Inputs and Output
 	input 			clk;
-	input 			feature;
+	input [31:0]	feature[41:0];
 	output 			gains;
 	output 			vad;
 
 	reg  			old_rx_data_rdy;
 	reg  [7:0]      char_data;
-	reg 			dense_out;
-	reg 			noise_input;
+	reg [31:0]		dense_out[23:0];
+	reg [31:0]		noise_input[89:0];
+	reg [31:0]		vad_gru_state[23:0];
 	reg 			denoise_input;
 
 	dense1	compute_dense1	( dense_out,			feature,			clk );
@@ -21,4 +22,4 @@ module RNN( gains, vad, feature, clk );
 		for ( i = 0;	i < 24;	i++ )	noise_input[i+24] = vad_gru_state[i];
 		for ( i = 0;	i < 42;	i++ )	noise_input[i+48] = feature[i];
 	endgenerate
-endmodule   
+endmodule
